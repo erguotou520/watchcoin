d6e89f4a0a52723220e44d436dd71116a8fc5a0a
erguotou <erguotou525@gmail.com>
2021-05-11 17:45:30 +0800

    Initial Commit

---
+++ watchcoin/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  watchcoin
+//
+//  Created by 二锅头 on 2021/5/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
